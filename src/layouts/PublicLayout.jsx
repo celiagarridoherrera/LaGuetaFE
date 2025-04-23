88bff8c00ebf86823b23950f01c39777d4719afd
Celia Garrido <celiagarridoherrera@gmail.com>
2025-04-24 01:48:48 +0200

    add NavbarPublic and Footer into PublicLayout

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,6 +1,15 @@
-/* import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import {PublicNavbar} from "../components/PublicNavbar";
+import {Footer} from "../components/Footer";
 
-export default function PublicLayout() {
+export const PublicLayout = () => {
     return (
         <div className="flex flex-col min-h-screen">
-             */
+          <PublicNavbar />
+            <main className="flex-grow">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    )
+}
